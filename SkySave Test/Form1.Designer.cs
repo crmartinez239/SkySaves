038a8d74e72da8dd9df396d975f3dea5c10eda34
Christopher Martinez <crmartinez239@protonmail.com>
2021-01-02 23:10:02 -0500

    WIP: Redesign of GUI

--- SkySave Test/Form1.Designer.cs
+++ SkySave Test/Form1.Designer.cs
@@ -28,63 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.eventsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openSaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeSaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewJSONToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportJSONToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.viewRawBitmapToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportBitmapToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(10, 267);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(282, 32);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Load Save";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(77, 10);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(150, 99);
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(10, 115);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(282, 146);
-            this.richTextBox1.TabIndex = 7;
-            this.richTextBox1.Text = "";
             // 
             // openFileDialog
             // 
             this.openFileDialog.Filter = "Skyrim save files|*.ess";
             // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 43);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(566, 787);
+            this.treeView1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eventsToolStripMenuItem,
+            this.toolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1145, 40);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // eventsToolStripMenuItem
+            // 
+            this.eventsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openSaveToolStripMenuItem,
+            this.closeSaveToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.eventsToolStripMenuItem.Name = "eventsToolStripMenuItem";
+            this.eventsToolStripMenuItem.Size = new System.Drawing.Size(72, 36);
+            this.eventsToolStripMenuItem.Text = "File";
+            // 
+            // openSaveToolStripMenuItem
+            // 
+            this.openSaveToolStripMenuItem.Name = "openSaveToolStripMenuItem";
+            this.openSaveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openSaveToolStripMenuItem.Size = new System.Drawing.Size(366, 44);
+            this.openSaveToolStripMenuItem.Text = "Open Save...";
+            this.openSaveToolStripMenuItem.Click += new System.EventHandler(this.openSaveToolStripMenuItem_Click);
+            // 
+            // closeSaveToolStripMenuItem
+            // 
+            this.closeSaveToolStripMenuItem.Name = "closeSaveToolStripMenuItem";
+            this.closeSaveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.closeSaveToolStripMenuItem.Size = new System.Drawing.Size(366, 44);
+            this.closeSaveToolStripMenuItem.Text = "Close Save";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(363, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(366, 44);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewJSONToolStripMenuItem,
+            this.exportJSONToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.viewRawBitmapToolStripMenuItem,
+            this.exportBitmapToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(90, 36);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // viewJSONToolStripMenuItem
+            // 
+            this.viewJSONToolStripMenuItem.Name = "viewJSONToolStripMenuItem";
+            this.viewJSONToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.viewJSONToolStripMenuItem.Text = "View JSON";
+            // 
+            // exportJSONToolStripMenuItem
+            // 
+            this.exportJSONToolStripMenuItem.Name = "exportJSONToolStripMenuItem";
+            this.exportJSONToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.exportJSONToolStripMenuItem.Text = "Export JSON";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(356, 6);
+            // 
+            // viewRawBitmapToolStripMenuItem
+            // 
+            this.viewRawBitmapToolStripMenuItem.Name = "viewRawBitmapToolStripMenuItem";
+            this.viewRawBitmapToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.viewRawBitmapToolStripMenuItem.Text = "View Raw Bitmap";
+            // 
+            // exportBitmapToolStripMenuItem
+            // 
+            this.exportBitmapToolStripMenuItem.Name = "exportBitmapToolStripMenuItem";
+            this.exportBitmapToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.exportBitmapToolStripMenuItem.Text = "Export Bitmap";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(304, 309);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1145, 845);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "SkySaves Test";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem eventsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openSaveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeSaveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewJSONToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportJSONToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem viewRawBitmapToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportBitmapToolStripMenuItem;
     }
 }
 
--- SkySave Test/Form1.cs
+++ SkySave Test/Form1.cs
@@ -20,59 +20,68 @@ namespace SkySave_Test
 
             if (saveFile != String.Empty)
             {
-                var saver = new SkySaves.SaveFile();
-                saver.Import(saveFile);
-                debug("Name", saver.HeaderInfo.PlayerName);
-                debug("Level", saver.HeaderInfo.PlayerLevel);
-                debug("Location", saver.HeaderInfo.PlayerLocation);
-                debug("Game Date", saver.HeaderInfo.GameDate);
-                debug("Race", saver.HeaderInfo.PlayerRace);
-                debug("Sex", saver.HeaderInfo.PlayerSexF);
-                debug("Version", saver.HeaderInfo.Version);
 
-                foreach (string plugIn in saver.PlugIns.PlugIns)
-                    debug("Plug In", plugIn);
+                //    richTextBox1.Text = string.Empty;
+                //    var saver = new SkySaves.SaveFile();
+                //    saver.Import(saveFile);
+                //    debug("Name", saver.HeaderInfo.PlayerName);
+                //    debug("Level", saver.HeaderInfo.PlayerLevel);
+                //    debug("Location", saver.HeaderInfo.PlayerLocation);
+                //    debug("Game Date", saver.HeaderInfo.GameDate);
+                //    debug("Race", saver.HeaderInfo.PlayerRace);
+                //    debug("Sex", saver.HeaderInfo.PlayerSexF);
+                //    debug("Version", saver.HeaderInfo.Version);
 
-                pictureBox1.Image = new Bitmap(saver.HeaderInfo.ScreenShot.ScreenShotImage,
-                    new Size(pictureBox1.Width, pictureBox1.Height));      
+                //    foreach (string plugIn in saver.PlugIns.PlugIns)
+                //        debug("Plug In", plugIn);
+
+                //    foreach (var miscStat in saver.MiscStats.Stats)
+                //        debug(miscStat.Name, miscStat.Value.ToString());
+
+                //    pictureBox1.Image = new Bitmap(saver.HeaderInfo.ScreenShot.ScreenShotImage,
+                //        new Size(pictureBox1.Width, pictureBox1.Height));
             }
         }
 
-        private void appendRedText(string text)
+        private void openSaveToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            richTextBox1.SelectionColor = Color.Red;
-            richTextBox1.AppendText($"{text}: ");
-            richTextBox1.SelectionColor = SystemColors.WindowText;
+
         }
 
-        private void debug(string title, string text)
-        {
-            appendRedText(title);
-            richTextBox1.AppendText($"{text}\n");
-        }
+        //private void appendRedText(string text)
+        //{
+        //    richTextBox1.SelectionColor = Color.Red;
+        //    richTextBox1.AppendText($"{text}: ");
+        //    richTextBox1.SelectionColor = SystemColors.WindowText;
+        //}
 
-        private void debug(string title, uint number)
-        {
-            appendRedText(title);
-            richTextBox1.AppendText($"{number.ToString()}\n");
-        }
+        //private void debug(string title, string text)
+        //{
+        //    appendRedText(title);
+        //    richTextBox1.AppendText($"{text}\n");
+        //}
 
-        private void debug(string title, byte b)
-        {
-            appendRedText(title);
-            richTextBox1.AppendText($"{b.ToString()}\n");
-        }
+        //private void debug(string title, uint number)
+        //{
+        //    appendRedText(title);
+        //    richTextBox1.AppendText($"{number.ToString()}\n");
+        //}
 
-        private void debug(string title, byte[] b)
-        {
-            appendRedText(title);
-            foreach(byte i in b)
-            {
-                richTextBox1.AppendText($"{i.ToString()} ");
-            }
+        //private void debug(string title, byte b)
+        //{
+        //    appendRedText(title);
+        //    richTextBox1.AppendText($"{b.ToString()}\n");
+        //}
 
-            richTextBox1.AppendText("\n");
-        }
+        //private void debug(string title, byte[] b)
+        //{
+        //    appendRedText(title);
+        //    foreach(byte i in b)
+        //    {
+        //        richTextBox1.AppendText($"{i.ToString()} ");
+        //    }
 
+        //    richTextBox1.AppendText("\n");
+        //}
     }
 }
